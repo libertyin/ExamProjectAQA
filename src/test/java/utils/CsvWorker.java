52a61a97a7f275339f55223f8fa72777f6de7989
Anna Kyriienko <anna.kyriienko11@gmail.com>
2021-05-18 18:57:46 +0300

    Added Csv Worker

---
+++ src/test/java/utils/CsvWorker.java
@@ -0,0 +1,42 @@
+package utils;
+
+import com.opencsv.CSVWriter;
+import io.qameta.allure.Step;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class CsvWorker {
+
+    @Step
+    public static void writeToCSVFile(String name, String email, String password, boolean append) throws IOException {
+        String[] data = new String[]{name, email, password};
+        String pathToCSV = "src/test/java/data/users/Users.csv";
+        try (CSVWriter write = new CSVWriter(
+                new FileWriter(pathToCSV, append),
+                '‡',
+                CSVWriter.NO_QUOTE_CHARACTER,
+                CSVWriter.DEFAULT_ESCAPE_CHARACTER,
+                CSVWriter.DEFAULT_LINE_END
+        )) {
+            write.writeNext(data);
+        }
+    }
+
+    public static Iterator<Object[]> parseCsvData(String pathToCSV) throws IOException {
+        File file = new File(pathToCSV);
+        ArrayList<Object[]> data = new ArrayList<>();
+        try (BufferedReader input = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = input.readLine()) != null) {
+                String[] temp = line.split("‡");
+                List<Object> array = new ArrayList<>(Arrays.asList(temp));
+                data.add(array.toArray());
+            }
+        }
+        return data.iterator();
+    }
+}
