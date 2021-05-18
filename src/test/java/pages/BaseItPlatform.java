2da42d3e77a028e89c2eb208c24fb08615a089e2
Anna Kyriienko <anna.kyriienko11@gmail.com>
2021-05-18 20:19:37 +0300

    Added test for broken links

--- src/test/java/pages/BaseItPlatform.java
+++ src/test/java/pages/BaseItPlatform.java
@@ -3,11 +3,12 @@ package pages;
 import pages.actions.MainPage;
 import pages.actions.RegisterPage;
 import pages.actions.SignInPage;
+import pages.actions.general.CommonSteps;
 import runner.Debug;
 
 public class BaseItPlatform extends Debug {
     public MainPage mainPage = new MainPage();
     public SignInPage signInPage = new SignInPage();
     public RegisterPage registerPage = new RegisterPage();
-
+    public CommonSteps commonSteps = new CommonSteps();
 }
--- src/test/java/tests/MainPageTests.java
+++ src/test/java/tests/MainPageTests.java
@@ -1,6 +1,43 @@
 package tests;
 
-import pages.actions.MainPage;
+import com.codeborne.selenide.Selenide;
+import data.ProjectUrls;
+import data.Users;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.BaseItPlatform;
 
-public class MainPageTests extends MainPage {
+import static com.codeborne.selenide.Selenide.back;
+import static com.codeborne.selenide.Selenide.open;
+
+public class MainPageTests extends BaseItPlatform {
+
+    @BeforeMethod
+    public void openProjectPage() {
+        open(ProjectUrls.IT_PLATFORM_WEBSITE.getUrl());
+    }
+
+    @Test
+    public void checkMenuBrokenLinks() {
+        mainPage.clickMenuLink(ProjectUrls.MENU_YOUR_OBJECTIVE.getUrl());
+        commonSteps.checkUrl(ProjectUrls.MENU_YOUR_OBJECTIVE.getUrl());
+        back();
+        mainPage.clickMenuLink(ProjectUrls.MENU_ACHIEVE_YOUR_GOAL.getUrl());
+        commonSteps.checkUrl(ProjectUrls.MENU_ACHIEVE_YOUR_GOAL.getUrl());
+        back();
+        mainPage.clickMenuLink(ProjectUrls.MENU_CHOOSE_YOUR_PACK.getUrl());
+        commonSteps.checkUrl(ProjectUrls.MENU_CHOOSE_YOUR_PACK.getUrl());
+        back();
+        mainPage.clickMenuLink(ProjectUrls.MENU_PRICING.getUrl());
+        commonSteps.checkUrl(ProjectUrls.MENU_PRICING.getUrl());
+        back();
+        mainPage.clickMenuLink(ProjectUrls.MENU_SIGN_IN.getUrl());
+        commonSteps.checkUrl(ProjectUrls.MENU_SIGN_IN.getUrl());
+    }
+
+    @AfterMethod
+    public void closeBrowser() {
+        Selenide.closeWebDriver();
+    }
 }
